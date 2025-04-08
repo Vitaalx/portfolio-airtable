240adbcbeed6a65a723c6fdffe7854f818ae752b
Maubry94 <matthieu.aubry94370@gmail.com>
2025-04-08 11:37:53 +0200

    init shadcn

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import TheButton from "./components/ui/button/TheButton.vue";
+
+function click() {
+	alert("HAH! You're dumb!");
+}
 </script>
 
 <template>
@@ -7,6 +12,10 @@ import { RouterView } from "vue-router";
     <h1 class="text-red-500">
       Portfolio created with Airtable.
     </h1>
+
+    <TheButton @click="click">
+      Click me!
+    </TheButton>
   </div>
   <RouterView />
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "node:path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
@@ -9,6 +10,11 @@ export default defineConfig({
 		tailwindcss(),
 		tsconfigPaths(),
 	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 	server: {
 		host: "0.0.0.0",
 		port: 3000,
