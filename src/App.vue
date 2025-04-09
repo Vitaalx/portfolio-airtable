44a8ba26c12b227031b066275d98f4116871466c
Liam MACQUAIRE <liamdu92@gmail.com>
2025-04-09 11:50:13 +0200

    save it

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
 import TheButton from "./components/ui/button/TheButton.vue";
+import { projectSchema } from "@/schemas/project";
 
-function click() {
-	alert("HAH! You're dumb!");
+async function click() {
+	await fetch("https://api.airtable.com/v0/appWVbhDdDZ3a6269/Project", {
+		method: "GET",
+		headers: {
+			Authorization: "Bearer patJ8epPt3vOFC64I.500b60268567f964c9eb47826da3b45230ed9b50bcb8853f9dd138bb9e6b5da4",
+			"Content-Type": "application/json",
+		},
+	})
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error("Network response was not ok");
+			}
+			return response.json();
+		})
+		.then((data: unknown) => {
+			const projects = projectSchema.parse(data);
+			projects.records.forEach((project) => {
+				console.log(project.fields.summary.value);
+			});
+		});
 }
 </script>
 
---
+++ src/schemas/project.ts
@@ -0,0 +1,50 @@
+import { z } from "zod";
+
+const thumbnailSchema = z.object({
+	url: z.string().url(),
+	width: z.number().positive(),
+	height: z.number().positive(),
+});
+
+const thumbnailsSchema = z.object({
+	small: thumbnailSchema,
+	large: thumbnailSchema,
+	full: thumbnailSchema,
+});
+
+const projectMediaItemSchema = z.object({
+	id: z.string(),
+	width: z.number().positive(),
+	height: z.number().positive(),
+	url: z.string().url(),
+	filename: z.string(),
+	size: z.number().positive(),
+	type: z.string(),
+	thumbnails: thumbnailsSchema,
+});
+
+const fieldsSchema = z.object({
+	title: z.string(),
+	description: z.string(),
+	status: z.string(),
+	image: z.array(projectMediaItemSchema),
+	studentNames: z.array(z.string()),
+	studentCount: z.number(),
+	technologyNames: z.array(z.string()),
+	technologyCount: z.number(),
+	nbLikes: z.number(),
+	summary: z.object({
+		value: z.string(),
+	}),
+	keywords: z.object({
+		value: z.string(),
+	}),
+});
+
+export const projectSchema = z.object({
+	records: z.array(z.object({
+		id: z.string(),
+		createdTime: z.string().datetime(),
+		fields: fieldsSchema,
+	})),
+});
